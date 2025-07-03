43d603f06633b600e964baf96e08212daca7024e
Fatima Yarhamidi <fatima.yarhamidi@gmail.com>
2025-07-03 14:44:40 +1200

    add matte bg

--- frontend/components/Toggle.jsx
+++ frontend/components/Toggle.jsx
@@ -13,51 +13,53 @@ export default function ToggleSwitch() {
   if (!mounted) return null
 
   return (
-    <div className="flex justify-end md:justify-start">
-      <div className=" md:pl-30 lg:pl-50 flex items-center space-x-2 relative text-black overflow-hidden lg:pb-4 md:pb-4 sm:pb-0">
-        {/* NZ Flag */}
-        <img
-          src="/nzFlag.svg"
-          alt="New Zealand Flag"
-          className={`w-6 h-6 md:w-10 md:h-8 transition-opacity duration-300 ${
-            isUK ? 'opacity-30' : 'opacity-100'
-          }`}
-        />
-        <div>
-          {/* Toggle Button */}
-          <div
-            onClick={toggleLocale}
-            className="w-10 h-4 md:w-16 md:h-7 flex items-center border-2  text-black rounded-full p-1 cursor-pointer transition-colors duration-300 relative"
-          >
-            {/* Text inside the toggle */}
-            <span
-              className={`absolute text-sm md:text-md font-[Convergence] transition-all duration-300 ${
-                isUK ? 'right-0 text-right pr-2' : 'left-0 text-left pl-2'
-              }`}
-            >
-              {/* {isUK ? 'UK' : 'NZ'} */}
-            </span>
-
-            {/* Circle */}
+    <div className="text-black bg-transparent backdrop-blur-md mx-2  lg:mx-10 xl:mx-50 px-2">
+      <div className="flex justify-end md:justify-start pb-2 ">
+        <div className="  flex items-center space-x-2 relative text-black overflow-hidden mt-2 lg:pb-0 md:pb-0 sm:pb-0">
+          {/* NZ Flag */}
+          <img
+            src="/nzFlag.svg"
+            alt="New Zealand Flag"
+            className={`w-6 h-6 md:w-10 md:h-8 transition-opacity duration-300 ${
+              isUK ? 'opacity-30' : 'opacity-100'
+            }`}
+          />
+          <div>
+            {/* Toggle Button */}
             <div
-              className={`w-3 h-3 md:w-5 md:h-5 border-1 text-black bg-[#8A5082] rounded-full shadow-md transform transition-transform duration-300 ${
-                !isUK
-                  ? '-translate-x-1 md:translate-x-0'
-                  : 'translate-x-5 md:translate-x-8 '
-              }`}
-            />
-          </div>
-        </div>
-        {/* UK Flag - Show only when isUK is true */}
+              onClick={toggleLocale}
+              className="w-10 h-4 md:w-16 md:h-7 flex items-center border-2  text-black rounded-full p-1 cursor-pointer transition-colors duration-300 relative"
+            >
+              {/* Text inside the toggle */}
+              <span
+                className={`absolute text-sm md:text-md font-[Convergence] transition-all duration-300 ${
+                  isUK ? 'right-0 text-right pr-2' : 'left-0 text-left pl-2'
+                }`}
+              >
+                {/* {isUK ? 'UK' : 'NZ'} */}
+              </span>
 
-        {/* UK Flag */}
-        <img
-          src="/ukFlag.svg"
-          alt="UK Flag"
-          className={`w-6 h-6 md:w-10 md:h-8 transition-opacity duration-300 ${
-            isUK ? 'opacity-100' : 'opacity-30'
-          }`}
-        />
+              {/* Circle */}
+              <div
+                className={`w-3 h-3 md:w-5 md:h-5 border-1 text-black bg-[#8A5082] rounded-full shadow-md transform transition-transform duration-300 ${
+                  !isUK
+                    ? '-translate-x-1 md:translate-x-0'
+                    : 'translate-x-5 md:translate-x-8 '
+                }`}
+              />
+            </div>
+          </div>
+          {/* UK Flag - Show only when isUK is true */}
+
+          {/* UK Flag */}
+          <img
+            src="/ukFlag.svg"
+            alt="UK Flag"
+            className={`w-6 h-6 md:w-10 md:h-8 transition-opacity duration-300 ${
+              isUK ? 'opacity-100' : 'opacity-30'
+            }`}
+          />
+        </div>
       </div>
     </div>
   )
