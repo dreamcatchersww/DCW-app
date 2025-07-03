a78483521b62811193c878b1f022f92c5d25e2c9
Fatima Yarhamidi <fatima.yarhamidi@gmail.com>
2025-07-03 13:49:48 +1200

    make navbar sticky

--- frontend/components/RootClientLayout.tsx
+++ frontend/components/RootClientLayout.tsx
@@ -34,17 +34,23 @@ export default function RootClientLayout({
       }
 
   return (
-    <div style={backgroundStyle}>
+    <div style={backgroundStyle} className="overflow-visible">
       <TopLogos />
 
-      <div className="flex items-center justify-between md:hidden px-6 top-0 z-50 bg-gradient-to-r from-[#8A5082] to-[#A5CAD2] py-1 mt-4">
+      <div className=" sticky flex items-center justify-between md:hidden px-6 top-0 z-50 bg-gradient-to-r from-[#8A5082] to-[#A5CAD2] py-1 mt-4">
         <MobileMenuToggle />
         <Toggle />
       </div>
 
-      {/* Desktop Navbar */}
-      <div className="lg:pt-5 pt-2 hidden md:block">
-        <Toggle />
+      {/* Sticky Toggle */}
+      <div className="sticky top-0 z-50 hidden md:block bg-white py-1 px-6">
+        <div className="">
+          <Toggle />
+        </div>
+      </div>
+
+      {/* Sticky Navbar under Toggle */}
+      <div className="sticky top-[48px] z-40 hidden md:block  py-2">
         <Navbar />
       </div>
 
